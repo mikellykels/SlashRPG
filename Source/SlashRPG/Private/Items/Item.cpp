cfa2e358dcdf4d9c045039c2e02d1fb4ca6e3a1b
Kelly Carino <mikaela.carino@gmail.com>
2023-08-08 04:16:56 -0700

    add item actor and debug macros

---
+++ Source/SlashRPG/Private/Items/Item.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+#include "Components/StaticMeshComponent.h"
+#include "SlashRPG/DebugMacros.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	RunningTime += DeltaTime;
+}
+
